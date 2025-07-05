ad540d2381dc3ebfe46cb5c493abbef50042830f
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-07-05 20:38:41 +0900

    開発ブランチの基盤構築を初回コミット

---
+++ src/test/java/io/github/Toku2001/trackandfieldapp/TrackandfieldApplicationTests.java
@@ -0,0 +1,11 @@
+package io.github.Toku2001.trackandfieldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackandfieldApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
