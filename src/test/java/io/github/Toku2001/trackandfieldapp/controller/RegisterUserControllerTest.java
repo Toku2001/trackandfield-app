3d5e6cc4a268dc0c3056f5b8866c95c65bd8aa4f
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-07-13 23:00:59 +0900

    ユーザー新規作成のテストケースを修正

--- src/test/java/io/github/Toku2001/trackandfieldapp/controller/RegisterUserControllerTest.java
+++ src/test/java/io/github/Toku2001/trackandfieldapp/controller/RegisterUserControllerTest.java
@@ -1,9 +1,11 @@
 package io.github.Toku2001.trackandfieldapp.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +18,7 @@ import io.github.Toku2001.trackandfieldapp.repository.UserMapper;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@MapperScan("io.github.Toku2001.trackandfieldapp.repository") 
 public class RegisterUserControllerTest {
 
     @Autowired
@@ -28,15 +31,16 @@ public class RegisterUserControllerTest {
     void registerUser_success() throws Exception {
         String json = """
         {
-          "userName": "testUser",
-          "userPassword": "password123",
-          "userMail": "test@example.com"
+		    "userName": "testUser",
+		    "userPassword": "password123",
+		    "userMail": "test@example.com"
         }
         """;
 
         mockMvc.perform(post("/auth/register-user")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
+        				.andDo(print()) 
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.userName").value("testUser"))
                 .andExpect(jsonPath("$.registerNumber").isNumber()); 
@@ -56,6 +60,39 @@ public class RegisterUserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
         	.andExpect(status().isBadRequest());	
+    }
+
+    @Test
+    void registerUser_SizeOver_UserPassword() throws Exception {
+        String json = """
+        {
+          "userName": "testUser",
+          "userPassword": "sizeOfUserPasswordMoreThan16",
+          "userMail": "test@example.com"
+        }
+        """;
+
+        mockMvc.perform(post("/auth/register-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+        	.andExpect(status().isBadRequest());	
+    }
+
+    @Test
+    void registerUser_Nothing_Annotation_Symbol_userMail() throws Exception {
+        String json = """
+        {
+          "userName": "testUser",
+          "userPassword": "password123",
+          "userMail": "testexample.com"
+        }
+        """;
+
+        mockMvc.perform(post("/auth/register-user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+		        .andExpect(status().isBadRequest())
+		        .andExpect(jsonPath("$.error").value("正しいメールアドレスの形式で入力してください"));
     }
     
     @Test
@@ -79,7 +116,7 @@ public class RegisterUserControllerTest {
     void registerUser_Nothing_UserPassword() throws Exception {
         String json = """
         {
-          "userName": "sizeOfUserNameMoreThan16",
+          "userName": "testUser",
           "userPassword": "",
           "userMail": "test@example.com"
         }
@@ -94,21 +131,21 @@ public class RegisterUserControllerTest {
 
     @Test
     void registerUser_fails_when_service_returns_zero() throws Exception {
-        // 前提：すでに登録済みのユーザーが存在
-        userMapper.registerUser("testUser", "password123", "test@example.com");
+    	
+        userMapper.registerUser("errorUser", "errorPassword", "errortest@example.com");
 
         String json = """
         {
-          "userName": "testUser",
-          "userPassword": "password123",
-          "userMail": "test@example.com"
+          "userName": "errorUser",
+          "userPassword": "errorPassword",
+          "userMail": "errortest@example.com"
         }
         """;
 
         mockMvc.perform(post("/auth/register-user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
-            .andExpect(status().isInternalServerError())
+        	.andExpect(status().isInternalServerError()) 
             .andExpect(jsonPath("$.error").value("登録できませんでした"));
     }
 }
