51826740094de05a4ce4a667a7287aa5ea50d43b
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-07-17 07:17:40 +0900

    CIによってテーブルが自動生成させるように設定

--- src/main/java/io/github/Toku2001/trackandfieldapp/entity/User_Info.java
+++ src/main/java/io/github/Toku2001/trackandfieldapp/entity/User_Info.java
@@ -1,5 +1,13 @@
 package io.github.Toku2001.trackandfieldapp.entity;
 
+// JPAアノテーション
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +15,27 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "user_info")
 public class User_Info {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer user_Id;
+
+    @Column(name = "user_name", nullable = false)
     private String user_Name;
+
+    @Column(name = "user_password", nullable = false)
     private String user_Password;
+
+    @Column(name = "user_mail", nullable = false, unique = true)
     private String user_Mail;
+
+    @Column(name = "user_role")
     private String user_Role;
+
+    @Column(name = "refresh_token")
     private String refresh_Token;
 }
