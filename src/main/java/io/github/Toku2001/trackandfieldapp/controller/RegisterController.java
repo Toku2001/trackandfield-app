019aaf14feb0123777edafd299fe8a0c379ce213
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-07-29 23:25:47 +0900

    サービスクラスでDBからの戻り値を検証しているため、コントローラでは例外処理を実装しないように修正する

--- src/main/java/io/github/Toku2001/trackandfieldapp/controller/RegisterController.java
+++ src/main/java/io/github/Toku2001/trackandfieldapp/controller/RegisterController.java
@@ -5,14 +5,12 @@ import java.util.Map;
 
 import jakarta.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import io.github.Toku2001.trackandfieldapp.dto.competition.RegisterCompetitionRequest;
 import io.github.Toku2001.trackandfieldapp.dto.training.RegisterTrainingRequest;
@@ -35,10 +33,6 @@ public class RegisterController {
             return ResponseEntity.badRequest().body(Map.of("error", errorMsg));
         }
 		int registerNumber = registerTrainingService.registerTraining(request);
-		if(registerNumber == 0) {
-			System.out.println("RegisterUser failed: 練習日誌登録が正常に完了しませんでした");
-			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
-		}
 		Map<String, Object> response = new HashMap<>();
     	response.put("result", registerNumber);
 		return ResponseEntity.ok(response);
@@ -52,10 +46,6 @@ public class RegisterController {
             return ResponseEntity.badRequest().body(Map.of("error", errorMsg));
         }
 		int registerCompetitionNumber = registerCompetitionService.registerCompetition(request);
-		if(registerCompetitionNumber == 0) {
-			System.out.println("Registercompetition failed: 競技会情報の登録が正常に完了しませんでした");
-			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
-		}
 		return ResponseEntity.ok(registerCompetitionNumber);
 	}
 }
