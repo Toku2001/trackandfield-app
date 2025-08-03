a7b8208bd68ec4a89a9fef54063470bc7827e20e
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-08-03 22:25:39 +0900

    ログイン画面 エラー内容がアラートに表示されるように修正

--- src/main/resources/static/login/login.js
+++ src/main/resources/static/login/login.js
@@ -1,36 +1,40 @@
-document.getElementById("submit").addEventListener("click", function(event) {
-    event.preventDefault();
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-    const userName = document.getElementById("userName").value.trim();
-	const userPassword = document.getElementById("userPassword").value;
+  const userName = document.getElementById("userName").value.trim();
+  const userPassword = document.getElementById("userPassword").value;
 
-	if (userName == "" && userPassword =="") {
-	        alert("ユーザー名またはパスワードを入力してください。");
-	        return;
-	    }
-    fetch('http://localhost:8080/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type' : "application/json"},
-		body: JSON.stringify({ userName, userPassword}),
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data != null && data.userName == userName) {
-			sessionStorage.setItem('userName', data.userName);
-            sessionStorage.setItem('token', data.accessToken);
-            window.location.href = PATHS.HOME_PAGE;
-        } else {
-            alert("ユーザー名またはパスワードが正しくありません。");
-        }
-    })
-    .catch(error => console.error('Error:', error));
+  if (userName === "" || userPassword === "") {
+    alert("ユーザー名またはパスワードを入力してください。");
+    return;
+  }
+
+  fetch('http://localhost:8080/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': "application/json" },
+    body: JSON.stringify({ userName, userPassword }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data != null && data.userName === userName) {
+      sessionStorage.setItem('userName', data.userName);
+      sessionStorage.setItem('token', data.accessToken);
+      window.location.href = PATHS.HOME_PAGE;
+    } else {
+      alert("ユーザー名またはパスワードが正しくありません。");
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("ユーザー名またはパスワードが正しくありません。");
+  });
 });
 
 // パスワード表示切替
 function showOrHide() {
-    const passwordField = document.getElementById("userPassword");
-    const checkbox = document.getElementById("showpassword");
-    passwordField.type = checkbox.checked ? "text" : "password";
+  const passwordField = document.getElementById("userPassword");
+  const checkbox = document.getElementById("showpassword");
+  passwordField.type = checkbox.checked ? "text" : "password";
 }
 
 // 新規登録画面へ遷移
@@ -40,5 +44,5 @@ document.getElementById("goUserCreateBtn").addEventListener("click", () => {
 
 // パスワード再設定画面へ遷移
 document.getElementById("goPasswordResetBtn").addEventListener("click", () => {
-    window.location.href = PATHS.PASSWORDRESET_PAGE;
+  window.location.href = PATHS.PASSWORDRESET_PAGE;
 });
