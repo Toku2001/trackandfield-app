68f127830a3bbb7955d8ef813fe606a4f3f8bb3c
NakamuraTokuhisa <pole0500tngwz@gmail.com>
2025-08-04 23:12:41 +0900

    練習日誌削除のメソッドの引数にオブジェクトではなくデータ型の変数を渡すように修正

--- src/main/java/io/github/Toku2001/trackandfieldapp/service/training/impl/DeleteTrainingServiceImpl.java
+++ src/main/java/io/github/Toku2001/trackandfieldapp/service/training/impl/DeleteTrainingServiceImpl.java
@@ -1,12 +1,13 @@
 package io.github.Toku2001.trackandfieldapp.service.training.impl;
 
+import java.time.LocalDate;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import io.github.Toku2001.trackandfieldapp.dto.training.DeleteTrainingRequest;
 import io.github.Toku2001.trackandfieldapp.dto.user.UserDetailsForToken;
 import io.github.Toku2001.trackandfieldapp.exception.DatabaseOperationException;
 import io.github.Toku2001.trackandfieldapp.repository.TrainingMapper;
@@ -21,11 +22,11 @@ public class DeleteTrainingServiceImpl implements DeleteTrainingService{
 	private final TrainingMapper trainingMapper;
 	
 	@Override
-    public int deleteTraining(DeleteTrainingRequest request) {
-		System.out.println("DeleteUserService received: " + request.getTrainingDate());
+    public int deleteTraining(LocalDate trainingDate) {
+		System.out.println("DeleteUserService received: " + trainingDate);
 	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 	    UserDetailsForToken userDetails = (UserDetailsForToken) authentication.getPrincipal();
-	    int deleteTrainingNumber = trainingMapper.deleteTraining(userDetails.getUserId(), request.getTrainingDate());
+	    int deleteTrainingNumber = trainingMapper.deleteTraining(userDetails.getUserId(), trainingDate);
         if (deleteTrainingNumber == 0) {
             throw new DatabaseOperationException("正しく練習日誌が削除されませんでした", new Exception());
         }
